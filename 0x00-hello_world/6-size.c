2842ed9298b1b0ac3b2bc8c6b41a9d5df13ac79c
Maynot2 <paulmanot@gmail.com>
2020-09-16 11:45:59 +0000

    Print types with byte sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %i byte(s)\n", sizeof(c));
+	printf("Size of a int: %i byte(s)\n", sizeof(i));
+	printf("Size of a long int: %i byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
+	printf("Size of a float: %i byte(s)\n", sizeof(f));
+	return (0);
+}
