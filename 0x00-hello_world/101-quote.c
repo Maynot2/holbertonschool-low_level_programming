3b29985a36f4f867337e54b1ce025020464dab67
Maynot2 <paulmanot@gmail.com>
2020-09-16 16:59:20 +0000

    ...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,9 @@
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t qlen = strlen(q);
 
-	write(2, q, (size_t)strlen(q));
+	write(2, q, qlen);
 
 	return (1);
 }
