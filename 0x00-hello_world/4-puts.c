2199fb89d724c42c6a254b8fdd318f02267e0d60
Maynot2 <paulmanot@gmail.com>
2020-09-16 10:04:01 +0000

    Print message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char text[] = "Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
