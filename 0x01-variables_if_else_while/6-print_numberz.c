36c1cc465b3e93d92b7f6872b9f3e280e55dbd92
Maynot2 <paulmanot@gmail.com>
2020-09-17 12:10:13 +0000

    Print single digit from 0 to 9 without using a char variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Where the program starts
+  *
+  * Return: 0
+  *
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar((char)num);
+	}
+	putchar('\n');
+
+	return (0);
+}
