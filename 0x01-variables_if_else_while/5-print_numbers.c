b013af17fdd7d4645844834f6ee3748c9589c4dc
Maynot2 <paulmanot@gmail.com>
2020-09-17 11:55:01 +0000

    Print single digits from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Where the program starts
+  *
+  * Return: 0
+  *
+  */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
