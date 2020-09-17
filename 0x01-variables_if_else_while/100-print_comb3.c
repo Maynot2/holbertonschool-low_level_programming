321fe42ff09ce0a0d330f2371d94fbadeb469e19
Maynot2 <paulmanot@gmail.com>
2020-09-17 13:56:35 +0000

    Prints all possible different combinations of two digits in growing order, coma separated

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Where the program starts
+  *
+  * Return: 0
+  *
+  */
+int main(void)
+{
+	int i;
+	int j;
+	int c = 0;
+
+	/* Using ASCII values for digit 0 to 9 */
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48 + c; j < 58; j++)
+		{
+			putchar((char)i);
+			putchar((char)j);
+			if (i == 56 && j == 57)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+		if (c == 8)
+			break;
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
