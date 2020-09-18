c5045ffb5ac6bf3b5d074bf73ab54987860cb2b9
Maynot2 <paulmanot@gmail.com>
2020-09-18 06:16:04 +0000

    Find solution

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,45 +9,41 @@
 int main(void)
 {
 	int i, j, k, l;
+	int ci = 0;
 	int cj = 0;
 	int ck = 0;
-	int cl = 0;
+	int skip = 0;
 
-	/* Using ASCII values for digit 0 to 9 */
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48 + cj; j < 58; j++)
 		{
-			for (k = 48 + ck; k < 58; k++)
-			{	
-				for (l = 48 + cl; l < 58; l++)
+			if (ci > 0)
+				skip = ci;
+			for (k = 48 + skip; k < 58; k++)
+			{
+				skip = (ck == 0) ? cj : 0;
+				for (l = 48 + skip; l < 58; l++)
 				{
 					if ((i == k && j == l) || (j == k && i == l))
-						continue; 
-
+						continue;
 					putchar((char)i);
 					putchar((char)j);
-					putchar(' ');	
+					putchar(' ');
 					putchar((char)k);
 					putchar((char)l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
 					putchar(',');
 					putchar(' ');
-
 				}
-				if (k == 57)
-					cl++;
-				if (l >= 58)
-					l = 0;
-			}
-			if (j == 57)
 				ck++;
-			if (k >= 58)
-				k = 0;
-		}
-		if (i == 57)
+			}
 			cj++;
-		if (j >= 58)
-			j = 0;
+			ck = 0;
+		}
+		ci++;
+		cj = 0;
 	}
 	putchar('\n');
 
