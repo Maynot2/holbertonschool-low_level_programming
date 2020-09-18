df3e45a00449164cd4ea7b94fa17a8bcc19c48f7
Maynot2 <paulmanot@gmail.com>
2020-09-18 15:38:12 +0000

    Shift second loop using previous loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,33 +16,27 @@ int main(void)
 	{
 		n1 = i / 10;
 		n2 = i % 10;
-	
-		for (j = 0; j < 100; j++) 
+
+		for (j = i + 1; j < 100; j++)
 		{
 			n3 = j / 10;
 			n4 = j % 10;
 
-			if ((n1 == n4 && n2 == n3) || (n1 == n3 && n2 == n4))
-				continue;
-
-			if ((n2 <= n4) && (n1 <= n3))
+			if (!(n1 == 0 && n2 == 0 && n3 == 0 && n4 == 1))
 			{
-				if (!(n1 == 0 && n2 == 0 && n3 == 0 && n4 == 1))
-				{
-					putchar(',');
-					putchar(' ');
-				}				
-
-				putchar(n1 + ascii_0);
-				putchar(n2 + ascii_0);
+				putchar(',');
 				putchar(' ');
-				putchar(n3 + ascii_0);
-				putchar(n4 + ascii_0);
 			}
 
-		}		
-	}	
+			putchar(n1 + ascii_0);
+			putchar(n2 + ascii_0);
+			putchar(' ');
+			putchar(n3 + ascii_0);
+			putchar(n4 + ascii_0);
+
+		}
+	}
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
