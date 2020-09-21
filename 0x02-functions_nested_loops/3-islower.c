32dcbb239f246c42c4041dba01927105eae5c934
Maynot2 <paulmanot@gmail.com>
2020-09-21 14:23:06 +0000

    Remove executable permissions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
-  * islower - Tests if a character c is lower case
+  * _islower - Tests if a character c is lower case
   * @c: The character to be tested
   *
   * Return: on success 1
   *         on error 0
   *
   */
-int islower(int c)
+int _islower(int c)
 {
 	int l;
 
