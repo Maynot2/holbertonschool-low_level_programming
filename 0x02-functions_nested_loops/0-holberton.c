4649026ce60756777129d58b068f9a3f94677520
Maynot2 <paulmanot@gmail.com>
2020-09-21 06:57:12 +0000

    Print Holberton using external function

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+
+*/
+/**
+  * main: Start of the program
+  *
+  * return: 0
+  *
+  */
+int main(void)
+{
+	char school[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(school[i]);
+	}
+
+	return (0);
+}
+
+
