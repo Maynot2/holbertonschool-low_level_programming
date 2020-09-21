e2cda2f1c4e05857707ae57abfcb7261f3d483db
Maynot2 <paulmanot@gmail.com>
2020-09-21 10:35:01 +0000

    Print alphabet using external function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+
+*/
+/**
+  * main: Start of the program
+  *
+  * return: 0
+  *
+  */
+int main(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
+
