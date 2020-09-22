1d35583a8b49003a26004c800eb940d495edbabf
Maynot2 <paulmanot@gmail.com>
2020-09-22 07:22:21 +0000

    Compute adding 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+  * add - Adds 2 integers a and b
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: a + b
+  *
+  */
+void add(int a, int b)
+{
+	return (a + b);
+}
+
+
