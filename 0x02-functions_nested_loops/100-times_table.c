021585150c65bc1c97f04ac82e3cb5fb843d3292
Maynot2 <paulmanot@gmail.com>
2020-09-22 17:09:26 +0000

    Computes and print times tables up to 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+
+/**
+  * print_space - Print a coma and x many blank spaces
+  * @x: A integer
+  *
+  * Return: void
+  *
+  */
+
+void print_space(int x)
+{
+	int i;
+
+	_putchar(',');
+	for (i = 0; i < x; i++)
+		_putchar(' ');
+}
+
+/**
+  * times_table - Prints times tables from 0 to n
+  * @n: Integer between 0 and 15
+  *
+  * Return: void
+  *
+  */
+
+void print_times_table(int n)
+{
+	int i, j, p;
+
+	if (n < 0 || n > 15)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				p = i * j; /* product as per time tables 0x0 0x1 0x2... */
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				else if ((i * j) > 99)
+				{
+					print_space(1);
+					_putchar('0' + ((p / 10) / 10));
+					_putchar('0' + ((p / 10) % 10));
+					_putchar('0' + (p % 10));
+				}
+				else if ((i * j) > 9)
+				{
+					print_space(2);
+					_putchar('0' + (p / 10));
+					_putchar('0' + (p % 10));
+
+				}
+				else
+				{
+					print_space(3);
+					_putchar('0' + (p % 10));
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
