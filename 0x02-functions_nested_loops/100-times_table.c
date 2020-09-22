045e288e48c9eeee6f03af124f8c85f7ff0ab1e1
Maynot2 <paulmanot@gmail.com>
2020-09-22 17:20:09 +0000

    Return void on exception

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,8 +30,8 @@ void print_times_table(int n)
 	int i, j, p;
 
 	if (n < 0 || n > 15)
-	{
-	}
+		return;
+
 	else
 	{
 		for (i = 0; i <= n; i++)
@@ -39,6 +39,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				p = i * j; /* product as per time tables 0x0 0x1 0x2... */
+
 				if (j == 0)
 				{
 					_putchar('0');
