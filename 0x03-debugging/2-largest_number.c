89c9c9a056a1c39ccd11ed48eea052b77a628d3c
Maynot2 <paulmanot@gmail.com>
2020-09-22 10:39:00 +0000

    Change logic

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = a;
 
 	if (a > b && a > c)
 	{
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
