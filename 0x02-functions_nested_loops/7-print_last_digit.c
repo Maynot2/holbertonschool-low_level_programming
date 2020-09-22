d1c1087b773593b116b3bf7ea7edb9baa70ce28d
Maynot2 <paulmanot@gmail.com>
2020-09-22 07:56:32 +0000

    Remove _abs function and integrate logic in print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,6 @@
 #include "holberton.h"
 
 /**
-  * _abs - Returns the absolute value of a number n
-  * @n: The number to be computed
-  *
-  * Return: n if n > 0
-  *         0 if = 0
-  *        -n if n < 0
-  *
-  */
-int _abs(int n)
-{
-	if (n > 0)
-		return (n);
-	if (n < 0)
-		return (-n);
-	return (0);
-}
-
-/**
   * print_last_digit - Prints the last digit of a number n
   * @n: The number to be computed
   *
@@ -27,8 +9,10 @@ int _abs(int n)
   */
 int print_last_digit(int n)
 {
-	int ld = (_abs(n) % 10);
+	int ld = (n % 10);
 
+	if (ld < 0)
+		ld = -ld;
 	_putchar('0' + ld);
 
 	return (ld);
