18c217ed2f0da884d10c50fea5e5051b4b903afd
Maynot2 <paulmanot@gmail.com>
2020-09-23 06:27:30 +0000

    Compute and print first 100 fibonacci numbers starting at 1, 2, ...

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+  * printi - Prints a positive integer character by character recursivly
+  * @n: A positive Integer
+  *
+  * Return: void
+  *
+  */
+void printi(unsigned long n)
+{
+	if (n / 10)
+		printi(n / 10);
+	_putchar('0' + (n % 10));
+}
+
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int i = 0;
+	unsigned long n1 = 1, n2 = 2, tmp;
+
+	printi(n1);
+	_putchar(',');
+	_putchar(' ');
+	printi(n2);
+
+	while (i < 98)
+	{
+		tmp = n2;
+		n2 += n1;
+		n1 = tmp;
+		_putchar(',');
+		_putchar(' ');
+		printi(n2);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
