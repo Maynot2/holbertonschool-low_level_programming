2ae10c839212cb621ab1a18808feab46a203b90a
Maynot2 <paulmanot@gmail.com>
2020-09-24 10:02:59 +0000

    Computes a x b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - Computes the multiplication of 2 integers, a and b 
+ * @a: An integer
+ * @b: An integer
+ *
+ * Return: Computed value of a x b
+ *
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
