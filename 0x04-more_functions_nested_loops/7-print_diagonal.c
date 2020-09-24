9d3c62a8aaa6093b407a286a9df087c627400fc1
Maynot2 <paulmanot@gmail.com>
2020-09-24 12:25:29 +0000

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Print a diagonal line given an integer n
+ * @n: An integer that set the length of the diagonal
+ *
+ * Return: Void
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
