5e76899958dff537ad2a2672b1217ffb8f8695ab
Maynot2 <paulmanot@gmail.com>
2020-09-24 10:14:05 +0000

    Fix typo and remove trailing white space

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * mul - Computes the multiplication of 2 integers, a and b 
+ * mul - Computes the multiplication of 2 integers, a and b
  * @a: An integer
  * @b: An integer
  *
@@ -9,7 +9,7 @@
  *
  */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
