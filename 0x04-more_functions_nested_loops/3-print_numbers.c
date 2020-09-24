1bafb1e700fbaf7a3aabdc8ea457efc2b3b5c9b4
Maynot2 <paulmanot@gmail.com>
2020-09-24 10:19:16 +0000

    Print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 1
+ *
+ * Return: Void
+ *
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; i <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
