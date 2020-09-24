06264d3f603c7e7a7d8f68d4ed83f958e96abbea
Maynot2 <paulmanot@gmail.com>
2020-09-24 09:50:29 +0000

    Find out if Chracter is digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * _isupper - Tests if a character c is uppercase
+  * @c: The character to be tested
+  *
+  * Return: on success 1
+  *         on error 0
+  *
+  */
+int _isdigit(int c);
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d)
+	{
+		if (c == l)
+			return (1);
+	}
+
+	return (0);
+
+}
