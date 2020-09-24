1a4896d669208cc0efc56f17e0fd5e2f88879ace
Maynot2 <paulmanot@gmail.com>
2020-09-24 18:52:59 +0000

    Add logic to take care of big int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,23 @@
 #include "holberton.h"
 
 /**
- * print_number - Print a number n without using the standard libray
+ * print_i - Print a positive number n without using the standard libray
+ * @n: A positive integer
+ *
+ * Return: Void
+ *
+ */
+
+void printi(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * print_number - Print a number n without using the standard libray even
+ * if larger then int limit
  * @n: An integer
  *
  * Return: Void
@@ -10,13 +26,19 @@
 
 void print_number(int n)
 {
+	int n_p1, n_p2;
+
+	n_p1 = n / 1000000000;
+	n_p2 = n % 1000000000;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n_p1 = -n_p1;
+		n_p2 = -n_p2;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (n_p1)
+		printi(n_p1);
+	printi(n_p2);
 }
