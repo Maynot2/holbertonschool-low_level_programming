189b2ca376ceef024fc710217f149585a92a0fac
Maynot2 <paulmanot@gmail.com>
2020-09-28 06:50:24 +0000

    Reset value of integer through pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * reset_to_98 - takes a pointer to an int n and updates the value it points
+  * to, to 98.
+  *	@n: A pointer to an int.
+  *
+  * Return: void.
+  *
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
