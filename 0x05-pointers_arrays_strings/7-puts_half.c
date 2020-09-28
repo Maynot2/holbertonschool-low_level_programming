ebe9f8c052e3d9c43d93a012262c7f4177ca458e
Maynot2 <paulmanot@gmail.com>
2020-09-28 14:22:54 +0000

    Print half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+/**
+ * _strlen - takes a string pointer s and return the length of the matching
+ * string.
+ *	@s: A pointer to a string.
+ *
+ * Return: An integer corresponding to the length of a string.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
+
+/**
+ * puts_half - Takes a string pointer s and prints half of the string followed
+ * by a new line.
+ * @str: A pointer to a string.
+ *
+ * Return: Void.
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i, sl = _strlen(str);
+	char *start;
+
+	start = str;
+
+	if (sl % 2 != 0) /* even characters length */
+	{
+		for (i = 0; i < sl / 2; i++)
+			start++;
+	}
+	else
+	{
+		for (i = 0; i < (sl + 1) / 2; i++)
+			start++;
+	}
+
+	i = 0;
+
+	while (*(start + i))
+	{
+		_putchar(*(start + i));
+		i++;
+	}
+	_putchar('\n');
+}
