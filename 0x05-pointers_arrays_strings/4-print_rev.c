a63bebd1c652015743d43902dbe21b27ff03eed0
Maynot2 <paulmanot@gmail.com>
2020-09-28 09:00:51 +0000

    Print like stdlib puts but in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _strlen - takes a string pointer s and return the length of the matching
+ * string.
+ *	@s: A pointer to a string.
+ *
+ * Return: An integer corresponding to the length of a string.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
+
+/**
+ * print_rev - takes a string pointer str and prints str to stdout in reverse
+ *	@str: A pointer to a string.
+ *
+ * Return: Void.
+ *
+ */
+
+void print_rev(char *str)
+{
+	int i;
+
+	for (i = _strlen(str); i >= 0; i--)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
