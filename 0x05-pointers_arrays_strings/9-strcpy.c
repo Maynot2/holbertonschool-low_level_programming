6eee2f4dc4ffd2750efb3246f6975b6c7d77f95a
Maynot2 <paulmanot@gmail.com>
2020-09-29 07:53:16 +0000

    Add null char at end of copied string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,23 +2,41 @@
 #include <stdio.h>
 
 /**
+ * _strlen - takes a string pointer s and return the length of the matching
+ * string.
+ * @s: A pointer to a string.
+ *
+ * Return: An integer corresponding to the length of a string.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
+
+/**
  * _strcpy - Takes a source char pointer *dest and copys it to a destination
  * char pointer
  * @src: A pointer to an array of chars.
  * @dest: A pointer to an array of chars.
  *
- * Return: Void.
+ * Return: A pointer to dest.
  *
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	while (*(src + i))
+	for (i = 0; i <= _strlen(src); i++)
 	{
 		*(dest + i) = *(src + i);
-		i++;
 	}
 
 	return (dest);
