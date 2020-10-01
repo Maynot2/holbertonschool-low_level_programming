3709a94e854f22d96dbde501539d891d5c93df59
Maynot2 <paulmanot@gmail.com>
2020-10-01 16:51:27 +0000

    Fix problematic shift

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -14,14 +14,15 @@ char *rot13(char *s)
 	int i = 0;
 	int key = 13;
 	int alpha_pos;
+	char abc[] = "abcdefghijklmnopqrstuvwxyz";
 
 	while (s[i])
 	{
 		if ((s[i] >= 'a' && s[i] <= 'z'))
 		{
-			alpha_pos = s[i] - 96;
+			alpha_pos = s[i] - 97;
 			alpha_pos = (alpha_pos + key) % 26;
-			s[i] = alpha_pos + 96;
+			s[i] = abc[alpha_pos];
 		}
 		i++;
 	}
