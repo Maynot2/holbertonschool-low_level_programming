c4b3da884606a4163524d4a391a38c8a9542e0ca
Maynot2 <paulmanot@gmail.com>
2020-10-02 03:44:05 +0000

    Incorporate +13 logic in second string with decode key

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,21 +11,18 @@
 
 char *rot13(char *s)
 {
-	int i = 0;
-	int key = 13;
-	int alpha_pos;
-	char abc[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0, j;
+	char abc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char abc13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (s[i])
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z'))
-		{
-			alpha_pos = s[i] - 97;
-			alpha_pos = (alpha_pos + key) % 26;
-			s[i] = abc[alpha_pos];
-		}
+		j = 0;
+		while (s[i] != abc[j])
+			j++;
+		if (j < 52)
+			s[i] = abc13[j];
 		i++;
 	}
-
 	return (s);
 }
