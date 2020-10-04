822f0f51909dc717a7795a24a5bd8ebbc934a401
Maynot2 <paulmanot@gmail.com>
2020-10-04 20:17:23 +0000

    Trying one last time

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include "holberton.h"
+#include <string.h>
+#include <ctype.h>
 
 /**
-  * str_replace - Replaces all escaped character of a string s of size size
-  * by dots.
-  * @s: A pointer to a string
-  * @size: An integer
-  *
-  * Return: Void.
-  */
+ * print_section - Print a given section of a string.
+ * by dots.
+ * @s: A pointer to a string
+ * @pos: An integer
+ * @len: An integer.
+ *
+ * Return: Void.
+ */
 
-void str_replace(char *s, int size)
+void print_section(char *s, int pos, int len)
 {
 	int i = 0;
 
-	while (i < size)
+	while (i < len)
 	{
-		if (*(s + i) < 32 || *(s + i) > 126)
-			*(s + i) = '.';
+		if (isprint(*(s + pos + i)))
+			putchar(*(s + pos + i));
+		else
+			putchar('.');
 		i++;
 	}
 }
 
-
 /**
-  * print_buffer - Prints the content of size bytes of the buffer pointed by b
-  * @b: A pointer to a string
-  * @size: An integer
-  *
-  * Return: Void.
-  */
+ * print_buffer - Prints the content of size bytes of the buffer pointed by b
+ * @b: A pointer to a string
+ * @size: An integer
+ *
+ * Return: Void.
+ */
 
 
 void print_buffer(char *b, int size)
 {
 	int i = 0, k = 0;
-
-	str_replace(b, size);
 
 	while (i < (size / 10))
 	{
@@ -45,10 +47,10 @@ void print_buffer(char *b, int size)
 		while (j < 10)
 		{
 			if (j == 0)
-				printf("00000%02x:", i * 10);
-			printf(" %02x%02x", *(b + (i * 10) + j), *(b + (i * 10) + j + 1));
+				printf("00000%02x: ", i * 10);
+			printf("%02x%02x ", *(b + (i * 10) + j), *(b + (i * 10) + j + 1));
 			if (j >= 8)
-				printf(" %-.10s", b + (i * 10));
+				print_section(b, i * 10, 10);
 			j += 2;
 		}
 		putchar('\n');
@@ -57,13 +59,13 @@ void print_buffer(char *b, int size)
 	while (k < 10)
 	{
 		if (k == 0)
-			printf("00000%02x:", i * 10);
+			printf("00000%02x: ", i * 10);
 		if (k < size % 10)
-			printf(" %2x%2x", *(b + (i * 10) + k), *(b + (i * 10) + k + 1));
+			printf("%02x%02x ", *(b + (i * 10) + k), *(b + (i * 10) + k + 1));
 		if (k >= size % 10)
 			printf("     ");
 		if (k >= 8)
-			printf(" %-.10s", b + (i * 10));
+			print_section(b, i * 10, size % 10);
 		k += 2;
 	}
 	putchar('\n');
