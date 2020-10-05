143b1c772f3bb26aedd73a03d12327644c96f65d
Maynot2 <paulmanot@gmail.com>
2020-10-05 10:35:51 +0000

    Take null char into account

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,24 +2,41 @@
 #include <stdio.h>
 
 /**
-  * _strchr - Locates a character c in a string s.
-  * @s: A pointer to a string.
-  * @c: A pointer to a character.
-  *
-  * Return: A pointer to first occurence of char c in string s.
-  *
-  */
+ * _strlen - takes a string pointer s and return the length of the matching
+ * string.
+ * @s: A pointer to a string.
+ *
+ * Return: An integer corresponding to the length of a string.
+ *
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*(s + c))
+		c++;
+
+	return (c);
+}
+
+/**
+ * _strchr - Locates a character c in a string s.
+ * @s: A pointer to a string.
+ * @c: A pointer to a character.
+ *
+ * Return: A pointer to first occurence of char c in string s.
+ *
+ */
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (*(s + i))
+	for (i = 0; i <= _strlen(s); i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
-		i++;
 	}
 	return (NULL);
-
 }
