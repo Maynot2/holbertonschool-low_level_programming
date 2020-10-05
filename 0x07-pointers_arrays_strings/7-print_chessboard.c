16bbc4c854e169e625fe869869e1970467bebc0e
Maynot2 <paulmanot@gmail.com>
2020-10-05 15:13:08 +0000

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Print a chessboard
+ * @a: A pointer to a an array.
+ *
+ * Return: Viod.
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
