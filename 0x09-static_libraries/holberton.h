e443edca0c2d8b23d63c8df8b9ab123fd379d6cb
Maynot2 <paulmanot@gmail.com>
2020-10-12 11:49:34 +0000

    Fix typo (l.2)

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,5 +1,5 @@
 #ifndef HEADER_FILE
-#define HEADER_FILEi
+#define HEADER_FILE
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +23,3 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
-
