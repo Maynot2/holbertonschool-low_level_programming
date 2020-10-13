c2f4974b21ca2c243003d606b1603f459b1765d0
Maynot2 <paulmanot@gmail.com>
2020-10-13 12:17:13 +0000

    Deal with grid[i] == NULL now

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,11 @@ int **alloc_grid(int width, int height)
 	int **grid = malloc(height * sizeof(int *));
 
 	for (i = 0; i < height; i++)
+	{
 		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+			return (NULL);
+	}
 
 	if (grid == NULL || width <= 0 || height <= 0)
 	{
