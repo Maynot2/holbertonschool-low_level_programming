df97edac95aeedd6b27fc8516e60e21f68613125
Maynot2 <paulmanot@gmail.com>
2020-10-14 13:26:57 +0000

    Fix output if s1 or s2 are NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -39,9 +39,11 @@ char *str_concat(char *s1, char *s2)
 	int i;
 
 	if (s1 == NULL)
-		*s1 = 0;
+	{
+		s1 = "";
+	}
 	if (s2 == NULL)
-		*s2 = 0;
+		s2 = "";
 	len1 = _strlen(s1);
 	len2 = _strlen(s2) + 1;
 
