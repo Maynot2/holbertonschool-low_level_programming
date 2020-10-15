85ac20d90bfd217e3d1158445c87fff5cc71e70b
Maynot2 <paulmanot@gmail.com>
2020-10-15 07:59:55 +0000

    Create function malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * malloc_checked - Allocates memory using malloc given an unsigned integer b.
+  * @b: An unsigned integer | the allocation size in bytes.
+  *
+  * Return: - On success: A void pointer to the allocated memory.
+  *         - On failure: Exit the program with a status of 98. 
+  *
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pt = malloc((size_t)b);
+
+	if (pt == NULL)
+		exit(98);
+	else
+		return (pt);
+}
