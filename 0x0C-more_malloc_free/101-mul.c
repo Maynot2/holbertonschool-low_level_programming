5fdb774975921eac7496517e34ca0b5da84f44f6
Maynot2 <paulmanot@gmail.com>
2020-10-15 18:20:37 +0000

    Uncomplete solution

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Where the program starts.
+ * @argc: An integer | the number of cmd line arguments passed to the program.
+ * @argv: An array of strings of all the program arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+	return (0);
+}
