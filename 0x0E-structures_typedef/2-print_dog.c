a02f1506399768ebf227889aef639ed41679db48
Maynot2 <paulmanot@gmail.com>
2020-10-19 14:34:13 +0000

    Change format of age in event of dog.age == NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-		d->age ? printf("Age: %f\n", d->age) : printf("Age: 0\n");
+		d->age ? printf("Age: %f\n", d->age) : printf("Age: 0.000000\n");
 		d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 }
