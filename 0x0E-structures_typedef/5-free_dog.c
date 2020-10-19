b24f0fe8bc84f2b6c404ee199e80c3cfec882097
Maynot2 <paulmanot@gmail.com>
2020-10-19 16:51:33 +0000

    Create function free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Free memory preiously allocated for a dog_t.
+ * @d: A dog_t
+ *
+ * Return: Nothing.
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
