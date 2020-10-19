08f6b77c83b2eb8a1bd66b9e79928c5fe1f721fc
Maynot2 <paulmanot@gmail.com>
2020-10-19 13:56:09 +0000

    Create function print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  * print_dog - Print data inside a dog struct.
+  * @d: A dog struct
+  *
+  * Return: Nothing.
+  *
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+		d->age ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+		d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
+	}
+}
+
