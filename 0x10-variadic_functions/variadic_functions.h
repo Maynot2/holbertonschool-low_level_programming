77d4e273ef1ff5b99546a7502e32cbc0f360e9e8
Maynot2 <paulmanot@gmail.com>
2020-10-24 12:24:02 +0000

    Add description to struct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,20 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
-typedef struct el
+/**
+ * struct format_specifier - Specifies a print format.
+ * @t: The type of format_specifier.
+ * @func: A pointer to a print function.
+ *
+ * Description: Specifies a print format.
+ */
+
+
+typedef struct format_specifier
 {
 	char t;
 	void (*func)(va_list list);
-} el_t;
+} fs_t;
 
 int _putchar(int);
 int sum_them_all(const unsigned int n, ...);
