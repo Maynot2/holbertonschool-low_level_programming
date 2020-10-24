50246352686fcbcedee006cecc13f4113cfdd42a
Maynot2 <paulmanot@gmail.com>
2020-10-24 08:35:32 +0000

    Create type def

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,12 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+typedef struct el
+{
+	char t;
+	void (* func)(va_list list);
+} el_t;
+
 int _putchar(int);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
