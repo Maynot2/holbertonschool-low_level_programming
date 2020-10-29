230668971f34e2bb4c02e87381dafdceb65ef301
Maynot2 <paulmanot@gmail.com>
2020-10-29 17:32:53 +0000

    Fix betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -21,7 +21,7 @@ int _strlen(char *s)
 }
 
 /**
- * add_node - Adds a node at the start of a linked list.
+ * add_node_end - Adds a node at the end of a linked list.
  * @head: A pointer to a pointer to the first element of the list.
  * @str: A string.
  *
@@ -52,8 +52,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	curr_node = *head;
-	while(curr_node->next)
-			curr_node = curr_node->next;
+	while (curr_node->next)
+		curr_node = curr_node->next;
 
 	curr_node->next = new_tail;
 
