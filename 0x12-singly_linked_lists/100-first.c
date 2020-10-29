e915553a29d4d98178bcee99ae1ca690217e025c
Maynot2 <paulmanot@gmail.com>
2020-10-29 18:31:40 +0000

    Prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+  * premain - Executes code before main.
+  *
+  * Return: Nothing.
+  *
+  */
+
+void __attribute__ ((constructor)) premain(void)
+{
+	    printf("You're beat! and yet, you must allow,\n");
+		printf("I bore my house upon my back!\n");
+}
