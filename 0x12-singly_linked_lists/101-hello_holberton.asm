05a601e81f53e78127b87f84a9d2f81c395a9ea5
Maynot2 <paulmanot@gmail.com>
2020-10-29 21:34:42 +0000

    Try different syntax

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,13 @@
 section .data
-	msg db "Hello, Holberton",15,10,0
+	msg db "Hello, Holberton",10,0
 section .text
 	extern printf
 	global main
+	
 	main:
-		push rbp
-		mov rbp, rsp
-
 		mov rdi, msg
+		mov rax, 0
 		call printf
-		
-		mov rsp, rbp
-		pop rbp
+
+		mov rax, 0
 		ret
