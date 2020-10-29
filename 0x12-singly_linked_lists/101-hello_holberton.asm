9c18db0cdf4799b4142a76e27a4ab2490fa84f32
Maynot2 <paulmanot@gmail.com>
2020-10-29 20:17:33 +0000

    Create hello holberton program in assembly with call to printf

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+	msg db "Hello, Holberton",15,10,0
+section .text
+	extern printf
+	global main
+	main:
+		push rbp
+		mov rbp, rsp
+
+		mov rdi, msg
+		call printf
+		
+		leave
+		ret
