4582d22ec725c4e1c9b8c45a2ef1896d2c1d73f6
Maynot2 <paulmanot@gmail.com>
2020-11-02 11:35:51 +0000

    Update function description

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - Frees a linked list of type listint_t & sets head to NULL.
+  * @head: A pointer to a pointer to the first elment of the list.
+  *
+  * Return: Nothing
+  *
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+
+	while (*head)
+	{
+		prev = *head;
+		*head = (*head)->next;
+		free(prev);
+	}
+}
