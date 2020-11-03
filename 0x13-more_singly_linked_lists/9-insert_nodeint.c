620cec3efdce6d354683310c503f49c981c50e1f
Maynot2 <paulmanot@gmail.com>
2020-11-03 19:42:28 +0000

    Deal with NULL *head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev, *current, *new;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || !(*head))
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
