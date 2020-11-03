b5811b6eb2970c3c3feaf93471d81196cf11f69b
Maynot2 <paulmanot@gmail.com>
2020-11-03 18:56:00 +0000

    Take care of cases when **head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev, *current, *new;
 	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
+
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
