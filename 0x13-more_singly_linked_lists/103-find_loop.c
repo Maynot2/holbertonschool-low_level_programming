03eadfa37ce797e3fe511b101a277800681c0c35
Maynot2 <paulmanot@gmail.com>
2020-11-03 13:10:12 +0000

    Create fnuction find_listint_loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Find the node at which a loop start if there is one.
+ * @head: Apointer to the start of the list.
+ *
+ * Return: The address of the node where the loop starts.
+ *         NULL if there is no loop.
+ *
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+
+	tortoise = head;
+	hare = head;
+	while (hare)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (hare == tortoise)
+		{
+			hare = head;
+			while(hare != tortoise)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+	}
+	return (NULL);
+}
