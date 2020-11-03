934f79939cdbbf87b6044aa17f30c4eb865a2d54
Maynot2 <paulmanot@gmail.com>
2020-11-03 19:21:14 +0000

    Deal with NULL head arg

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,24 +1,23 @@
 #include "lists.h"
 
 /**
-  * insert_nodeint_at_index - Insert a new node at a given index.
-  * @head: A pointer to a pointer the  head of the linked list.
-  * @idx: The index at which the new node is to be inserted.
-  * @n: The value of the new node as an integer.
-  *
-  * Return: The address of the new node on success.
-  *         NULL on failure
-  *
-  */
+ * insert_nodeint_at_index - Insert a new node at a given index.
+ * @head: A pointer to a pointer the  head of the linked list.
+ * @idx: The index at which the new node is to be inserted.
+ * @n: The value of the new node as an integer.
+ *
+ * Return: The address of the new node on success.
+ *         NULL on failure
+ *
+ */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *prev, *current, *new;
 	unsigned int i = 0;
-
-	if (!head || idx < 0)
+	
+	if(!head)
 		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
