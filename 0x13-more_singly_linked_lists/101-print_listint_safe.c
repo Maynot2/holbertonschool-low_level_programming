f4a71b474cb2ba0c60d3dba346c0e78f0dff321c
Maynot2 <paulmanot@gmail.com>
2020-11-03 09:32:01 +0000

    Create function print_safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+size_t distance_from_start(const listint_t *h, const listint_t *curr)
+{
+	size_t c = 0;
+
+	while(h)
+	{
+		if (h == curr)
+			break;
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
+
+/**
+ * print_listint_safe - Prints a linked list of type listint_t only once (even
+ * if the list is circular).
+ * @head: The head of the list.
+ *
+ * Return: The number of printed elements.
+ *
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *current;
+
+	current = head;
+	while (distance_from_start(head, current) == count && current)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}
