354eb21de63a1b80d96b657494e3b98972fa7889
Maynot2 <paulmanot@gmail.com>
2020-11-05 17:35:31 +0000

    Incomplete solution

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * get_endianness -  Checks the endianness of a computer.
+  *
+  * Return: 1 if big endian.
+  *         0 if little endian.
+  *
+  */
+
+int get_endianness(void)
+{
+	return (1);
+}
