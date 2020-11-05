028858aac779b18ebce572a5d2bad3f645b75ebc
Maynot2 <paulmanot@gmail.com>
2020-11-05 16:20:06 +0000

    Change failure condition

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,32 +1,6 @@
 #include "holberton.h"
 
 /**
- * bin_len - Computes the length of the binary convertion of an unsigned long
- * integer.
- * @num: An unsigned long integer
- *
- * Return: the length of the binary.
- *
- */
-
-unsigned int bin_len(unsigned long int num)
-{
-	unsigned int len = 0;
-
-	if (num == 0)
-		return (1);
-
-	while (num / 2)
-	{
-		len++;
-		num /= 2;
-	}
-	if (num % 2)
-		len++;
-	return (len);
-}
-
-/**
  * get_bit - Computes the value of a bit at a given index.
  * @n: A unsigned long integer.
  * @index: The given index as an unsigned integer
@@ -38,7 +12,7 @@ unsigned int bin_len(unsigned long int num)
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= bin_len(n))
+	if (index > 63)
 		return (-1);
 
 	return ((n & (1 << index)) >> index);
