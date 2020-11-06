7e9905b76d16d1e9481ab618852fb255d762c87f
Maynot2 <paulmanot@gmail.com>
2020-11-06 12:49:05 +0000

    Create function get_endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
 
 /**
-  * get_endianness -  Checks the endianness of a computer.
-  *
-  * Return: 1 if big endian.
-  *         0 if little endian.
-  *
-  */
+ * get_endianness -  Checks the endianness of a computer.
+ *
+ * Return: 1 if big endian.
+ *         0 if little endian.
+ *
+ */
 
 int get_endianness(void)
 {
-	return (1);
+	int num = 1;
+
+	if (*(char *)&num == 1)
+		return (1);
+	else 
+		return (0);
 }
