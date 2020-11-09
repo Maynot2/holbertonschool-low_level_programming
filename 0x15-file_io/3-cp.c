11e5ac76eabd7c92bb569a1550ea5a485130c967
Maynot2 <paulmanot@gmail.com>
2020-11-09 17:22:17 +0000

    Fix typo miising minus in front of one

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -89,7 +89,7 @@ int main(int argc, char **argv)
 
 	do {
 		read_c = read(fd_from, buff, 1024);
-		if (read_c == 1)
+		if (read_c == -1)
 			error_read(file_from);
 		wrote_c = write(fd_to, buff, read_c);
 		if (wrote_c != read_c)
