dd46148121f3e077a44d2d49e337190f3426f04f
Maynot2 <paulmanot@gmail.com>
2020-11-09 11:14:09 +0000

    Create function read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+  * read_textfile - Reads a text file and prints it to standard output.
+  * @filename: The file name
+  * @letters: The number of letters to be read.
+  *
+  * Return: The maximum number of characters that could have beeen read.
+  *
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buff;
+	int readd, wrote;
+
+	if (fd < 0)
+		return (0);
+
+	buff = malloc((sizeof(char) + 1) * letters);
+	if (!buff)
+		return (0);
+	buff[letters] = '\0';
+
+	readd = read(fd, buff, letters);
+	if (readd <= 0)
+		return (0);
+	write(STDOUT_FILENO, buff, letters);
+
+	close(fd);
+	free(buff);
+	return (readd);
+}
