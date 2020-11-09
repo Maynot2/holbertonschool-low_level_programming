9676d7fd6aa5c98d5841ee73a0a48df6a1db048e
Maynot2 <paulmanot@gmail.com>
2020-11-09 17:40:31 +0000

    Incomplete solution

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - Where the program starts.
+ * @argc: The number of arguments.
+ * @argv: The arguments as strings.
+ *
+ * Return: always 0
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	void(argc);
+	void(argv);
+
+	return (0);
+}
