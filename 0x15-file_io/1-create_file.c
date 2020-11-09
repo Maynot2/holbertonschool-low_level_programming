0ed8173ccc02b1aa117771d539f66c4b5eee91a1
Maynot2 <paulmanot@gmail.com>
2020-11-09 13:19:58 +0000

    Create function create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Computes the size of a string s.
+ * @s: A pointer to a string.
+ *
+ * Return: A integer.
+ *
+ */
+
+int _strlen(const char *s)
+{
+	unsigned int c = 0;
+
+	if (s == NULL)
+		return (c);
+
+	while (*(s + c))
+		c++;
+	return (c);
+}
+
+/**
+ * create_file - Creates a file.
+ * @filename: The file name
+ * @text_content: The content for the file.
+ *
+ * Return: 1 on success.
+ *        -1 on error.
+ *
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	fd = open(filename, O_TRUNC | O_WRONLY);
+	if (fd < 0)
+		fd = open(filename, O_CREAT | O_WRONLY, S_IWUSR + S_IRUSR);
+	if (fd < 0)
+		return (-1);
+
+	write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+	return (0);
+}
