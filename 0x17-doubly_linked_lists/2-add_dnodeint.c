bd21a516d86d1294b5cc4224370866065d4782b6
Maynot2 <paulmanot@gmail.com>
2020-12-11 11:39:01 +0000

    Add missing parentheses around return value

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,11 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new = NULL;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
