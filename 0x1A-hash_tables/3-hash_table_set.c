3fdc37c4748e579ae1ce6c4df9d0975528d8259a
Maynot2 <paulmanot@gmail.com>
2021-01-14 13:59:39 +0000

    Create function hash_table_set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table.
+ * @key: The key.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 on success
+ *         0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = NULL;
+	unsigned long int index;
+	char *v;
+
+	if (!key || !ht)
+		return (0);
+	v = strdup(value);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index])
+	{
+		node = ht->array[index];
+		while (node)
+		{
+			if (node->key == key)
+			{
+				node->value = v;
+				return (1);
+			}
+			node = node->next;
+		}
+	}
+
+	node = malloc(sizeof(hash_node_t *));
+	if (!node)
+		return (0);
+
+	node->key = (char *)key;
+	node->value = v;
+	if (ht->array[index])
+		node->next = ht->array[index];
+	else
+		node->next = NULL;
+	ht->array[index] = node;
+
+	return (1);
+}
