7171568d102fa24859bc57b481cb43cbdab0fe53
Maynot2 <paulmanot@gmail.com>
2021-01-15 10:25:30 +0000

    Add == 0

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -23,7 +23,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		node = ht->array[index];
 		while (node)
 		{
-			if (strcmp(node->key, key))
+			if (strcmp(node->key, key) == 0)
 				return (node->value);
 			node = node->next;
 		}
