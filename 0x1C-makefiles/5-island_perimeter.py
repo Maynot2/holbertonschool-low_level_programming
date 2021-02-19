3dceb4a4735eabcaaff6a4748d7715838a276649
Maynot2 <paulmanot@gmail.com>
2021-02-19 12:47:08 +0000

    Deal with islands touching the side of the grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,13 +8,13 @@
 def neighbours(grid, i, j):
     """Calculate the number of 1 neighbours a 1 cell has"""
     count = 0
-    if grid[i][j + 1] == 1:
+    if j < len(grid[i]) and grid[i][j + 1] == 1:
         count += 1
-    if grid[i][j - 1] == 1:
+    if j > 0 and grid[i][j - 1] == 1:
         count += 1
-    if grid[i + 1][j] == 1:
+    if i < len(grid) and grid[i + 1][j] == 1:
         count += 1
-    if grid[i + 1][j] == 1:
+    if i > 0 and grid[i - 1][j] == 1:
         count += 1
     return count
 
