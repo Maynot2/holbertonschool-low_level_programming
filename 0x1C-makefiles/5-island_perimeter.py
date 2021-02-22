b78408d479946687e4bb6bbc4d40d31d91bb9ebf
Maynot2 <paulmanot@gmail.com>
2021-02-22 08:15:37 +0000

    Fix Island on the side of the map

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,11 +8,11 @@
 def neighbours(grid, i, j):
     """Calculate the number of 1 neighbours a 1 cell has"""
     count = 0
-    if j < len(grid[i]) and grid[i][j + 1] == 1:
+    if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
         count += 1
     if j > 0 and grid[i][j - 1] == 1:
         count += 1
-    if i < len(grid) and grid[i + 1][j] == 1:
+    if i < len(grid) - 1 and grid[i + 1][j] == 1:
         count += 1
     if i > 0 and grid[i - 1][j] == 1:
         count += 1
