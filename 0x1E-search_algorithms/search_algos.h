87f1fbfb595d6808e657a48da3a26407cc332ae3
Maynot2 <paulmanot@gmail.com>
2021-04-19 06:19:08 +0000

    Add header with protos

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prottotypes */
+
+int linear_search(int *, size_t, int);
+int binary_search(int *, size_t, int);
+
+#endif
