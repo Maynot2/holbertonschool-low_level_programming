67e6463da6716e63c04a8228d29ebc387578073b
Maynot2 <paulmanot@gmail.com>
2021-04-21 07:28:58 +0000

    Add data structure

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /* Prottotypes */
 
 int linear_search(int *, size_t, int);
