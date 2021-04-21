aaf0990716a6fb570de7e46331d47a4775ac892e
Maynot2 <paulmanot@gmail.com>
2021-04-21 12:07:57 +0000

    Add proto and data structure def

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,12 +14,33 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct listint_s
 {
     int n;
     size_t index;
     struct listint_s *next;
 } listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
 
 /* Prottotypes */
 
@@ -30,5 +51,6 @@ int interpolation_search(int *, size_t, int);
 int exponential_search(int *, size_t, int);
 int advanced_binary(int *, size_t, int);
 listint_t *jump_list(listint_t *, size_t, int);
+skiplist_t *linear_skip(skiplist_t *, int);
 
 #endif
